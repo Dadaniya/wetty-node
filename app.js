565a04b9c8b48205f163b661ae65c1a2c4046ed6
nizhiwang <18951367127>
2016-03-27 20:55:05 +0800

    first

---
+++ app.js
@@ -0,0 +1,61 @@
+/**
+ * Created by nzw on 2016/3/24.
+ */
+var process = require('child_process').exec;
+var http = require('http');
+var fs=require('fs');
+var os=require('os');
+var url=require('url');
+var encoding='cp936';
+var binaryEncoding='binary';
+var count=0;
+var iconv;
+
+/*
+ *֧��linux
+ * windows
+ */
+if(/win/.test(os.platform())){
+    iconv=require('iconv-lite');
+}
+else{
+    iconv=require('iconv');
+}
+
+/*
+ * 过滤危险命令
+ *
+ */
+function filter(str){
+    if(/rm|delete|format/gi.test(str)){return "echo  cmd not supported";}
+    return str;
+}
+/*
+ *启一个服务web访问
+ */
+http.createServer(function (req, res) {
+    if(url.parse(req.url).pathname=='/'){
+        console.log('/');
+        fs.readFile('index.html', function (err, data) {
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.end(data);
+        })
+    }
+    else if(url.parse(req.url).pathname=='/COM'){
+        console.log(url.parse(req.url,true).query.cmd+":"+filter(url.parse(req.url,true).query.cmd));
+        process(filter(url.parse(req.url,true).query.cmd),{encoding:'binary'}, function (err, stdout, stderr) {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end(iconv.decode(new Buffer(stdout,binaryEncoding),encoding));
+            }
+        );
+    }
+
+    else{
+        res.writeHead(404,{'Content-Type':'text/html'});
+        res.end('404 error! ');
+    }
+
+
+
+}).listen(8844);
+console.log('server running...')
